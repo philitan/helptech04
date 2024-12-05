5cb0dc5c0971aef2223295b0e575c3a0f03740b5
KanataOkumura <okumura.kanata.971@s.kyushu-u.ac.jp>
2024-12-05 18:01:07 +0900

    シミュレーションの項目の追加

--- resources/views/simulation/index.blade.php
+++ resources/views/simulation/index.blade.php
@@ -8,8 +8,21 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <div  style="font-size: 20px;" class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("シミュレーション") }}
+
+                </div>
+                <div z style="font-size: 20px;" class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("雇用形態") }}
+
+                </div>
+                <div  style="font-size: 20px;" class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("ツール代") }}
+
+                </div>
+                <div z style="font-size: 20px;" class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("備品代") }}
+
                 </div>
             </div>
         </div>
