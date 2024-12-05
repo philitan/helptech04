02c6b9ec97751c0aa16a88dee6519bd8db99dadb
KanataOkumura <okumura.kanata.971@s.kyushu-u.ac.jp>
2024-12-05 20:26:24 +0900

    実行ボタンと雇用形態のスライドボタンの追加

--- resources/views/simulation/index.blade.php
+++ resources/views/simulation/index.blade.php
@@ -8,22 +8,41 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <form>
                 <div  style="font-size: 20px;" class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("シミュレーション") }}
 
                 </div>
-                <div z style="font-size: 20px;" class="p-6 text-gray-900 dark:text-gray-100">
+                <div  style="font-size: 20px;" class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("雇用形態") }}
-
+                    <div class="flex items-center">
+                        <label for="toggle" class="relative cursor-pointer">
+                            <input type="checkbox" id="toggle" class="sr-only peer" onchange="toggleText(this)">
+                            <div class="w-10 h-6 bg-gray-300 rounded-full peer-checked:bg-blue-500 peer-focus:ring-2 peer-focus:ring-blue-500 transition"></div>
+                            <div class="w-4 h-4 bg-white rounded-full absolute top-1 left-1 peer-checked:translate-x-4 transition"></div>
+                        </label>
+                        <span id="toggle-text" class="ml-3 text-gray-900 dark:text-gray-100">フルタイム</span>
+                    </div>
+                    <script>
+                        function toggleText(toggle) {
+                            const text = toggle.checked ? 'パートタイム' : 'フルタイム';
+                            document.getElementById('toggle-text').innerText = text;
+                        }
+                    </script>
                 </div>
                 <div  style="font-size: 20px;" class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("ツール代") }}
 
-                </div>
-                <div z style="font-size: 20px;" class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("備品代") }}
 
                 </div>
+                <div  style="font-size: 20px;" class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("備品代") }}
+                    
+
+                </div>
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">シミュレーションを実行
+                </button>
+                </form>
             </div>
         </div>
     </div>
