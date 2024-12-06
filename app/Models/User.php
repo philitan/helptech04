7f07bbd1fe9c5471f218c60cf856ad24fc39e871
“yuki” <yuki12240221@au.com>
2024-12-07 01:32:12 +0900

    ツール一覧と追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function tools()
+    {
+        return $this->hasMany(Tools::class);
+    }
 }
