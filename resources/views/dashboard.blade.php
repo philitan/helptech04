7a791fbb0232859c899f1896f183811e7424c52a
philitan <tyeru.anrarge.2467@gmail.com>
2024-12-06 22:11:44 +0900

    計算できたかも

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,7 @@
+<?php
+use App\Models\Insurance;
+?>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -14,12 +18,35 @@
             <!-- 以下、計算のテスト用 -->
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <?php
-                    use App\Models\Insurance;
+                    // 本来は月給を入れる場所、テスト用なので適当に値を入れている
+                    $gekyuu = 100000;
+                    // 標準報酬のカラムのみ取得
+                    $insurances = Insurance::select('salary')->get();
+                    // dd($insurances);
+                    // 月給と標準報酬を比較して最適な等級(=ID)を見つけ出す
+                    $id = 0;
+                    $next = 0;
+                    foreach($insurances as $salary){
+                        $next = $salary->salary;
+                        if($gekyuu < $next){
+                            break;
+                        }
+                        $id++;
+                    }
 
-                    $kihon = 100000;
-                    $insurance = Insurance::where('salary', '<=', "$kihon")->get();
-                    $insurance2 = $insurance::first();
-                    echo $kihon;
+                    // 見つけ出したIDで今度はその行のみ取得
+                    $insurances2 = Insurance::find($id);
+
+                    // 社会保険料の計算をする(ひとまず介護保険なしの場合のみ、介護保険はifで分岐すれば出来る)
+                    // 労働者と事業主両方を合わせて計算しているため、企業側が何を求めるかによって変わる可能性あり
+                    $health = $insurances2->health;
+                    $welfare = $insurances2->welfare;
+                    $employment = $gekyuu*0.0155;
+
+                    $kekka = $gekyuu + $health + $welfare + $employment;
+                    // 出力
+                    echo '月給：'.$gekyuu."<br>";
+                    echo '社会保険+雇用保険料込み：'.$kekka;
                     ?>
                 </div>
             <!-- テスト用ここまで -->
