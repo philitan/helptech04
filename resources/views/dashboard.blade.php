799bcfdc47bf6b081e51317eaae1ea48154247a5
philitan <tyeru.anrarge.2467@gmail.com>
2024-12-10 21:10:31 +0900

    社会保険料テーブル完成

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,13 +18,14 @@ use App\Models\Insurance;
             <!-- 以下、計算のテスト用 -->
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <?php
-                    // 本来は月給を入れる場所、テスト用なので適当に値を入れている
-                    $gekyuu = 100000;
+                    // 本来は月給、交通費、年齢を入力情報から取得する、今回は計算のテスト用なので適当に値を入れている
+                    $gekyuu = 200000;
                     $koutuu = 10000;
+                    $age = 25;
                     $goukei = $gekyuu+$koutuu;
+
                     // 標準報酬のカラムのみ取得
                     $insurances = Insurance::select('salary')->get();
-                    // dd($insurances);
                     // 月給と標準報酬を比較して最適な等級(=ID)を見つけ出す
                     $id = 0;
                     $next = 0;
@@ -38,17 +39,23 @@ use App\Models\Insurance;
 
                     // 見つけ出したIDで今度はその行のみ取得
                     $insurances2 = Insurance::find($id);
-
-                    // 社会保険料の計算をする(ひとまず介護保険なしの場合のみ、介護保険はifで分岐すれば出来る)
-                    // 労働者と事業主両方を合わせて計算しているため、企業側が何を求めるかによって変わる可能性あり
-                    $health = $insurances2->health;
+                    // 社会保険料の計算をする
+                    if($age >= 40){
+                        $health = $insurances2->health_care;
+                    } else {
+                        $health = $insurances2->health;
+                    }
                     $welfare = $insurances2->welfare;
                     $employment = $goukei*0.0155;
 
                     $kekka = $goukei + $health + $welfare + $employment;
+                    $kekka = (int)$kekka;
+
                     // 出力
-                    echo '月給：'.$goukei."<br>";
-                    echo '社会保険+雇用保険料込み：'.$kekka;
+                    echo '月給：'.$gekyuu."<br>";
+                    echo '交通費：'.$koutuu."<br>";
+                    echo '年齢：'.$age."歳<br>";
+                    echo '社会保険料込み：'.$kekka;
                     ?>
                 </div>
             <!-- テスト用ここまで -->
