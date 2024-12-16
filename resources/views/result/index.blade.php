1a520b4cb03a7b98e9c6ddfd07fe7b6dbba62e07
philitan <tyeru.anrarge.2467@gmail.com>
2024-12-16 17:45:35 +0900

    送信された情報の表示

--- resources/views/result/index.blade.php
+++ resources/views/result/index.blade.php
@@ -1,3 +1,7 @@
+<?php
+use App\Models\Tools;
+?>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -10,7 +14,25 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                 <?php
-                    dd($_POST);
+                    // dd($_POST);
+                    foreach($_POST as $key => $value){
+                        if($key === "tool-cost"){
+                            echo "tool-cost {<br>";
+                            foreach($value as $key2 => $value2){
+                                $tools = Tools::where('name', '=', $value2)->get();
+                                foreach($tools as $tool){
+                                    $price = $tool->price;
+                                    echo "　".$value2." = ".(int)$price."円<br>";
+                                }
+                            }
+                            echo "}<br>";
+                        }
+                        else if($key !== "_token"){
+                            if(!empty($value)){
+                                echo $key." = ".$value."円<br>";
+                            }
+                        }
+                    }
                 ?>
                 </div>
             </div>
--- resources/views/simulation/index.blade.php
+++ resources/views/simulation/index.blade.php
@@ -1,6 +1,7 @@
 <?php
 use App\Models\Tools;
 ?>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
