d34b3642f6ff61216b7f6137f33368958c90cfd9
philitan <tyeru.anrarge.2467@gmail.com>
2024-12-23 19:50:36 +0900

    仮の手取り入力

--- resources/views/result/index.blade.php
+++ resources/views/result/index.blade.php
@@ -15,143 +15,94 @@ use App\Models\Insurance;
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                 <?php
-                    //フルタイムの時の処理
+                    // 入力されたデータの取得
                     if ($_POST["employment-type"] === "fulltime") {
-                        // dd($_POST);
-
-                        // 入力されたデータの取得
+                        // フルタイム
                         $monthly = $_POST["monthly-salary"] * 10000; // 月給(万円を円に直す)
                         $traffic = $_POST["commute-cost"]; // 交通費
                         $age = $_POST["age"]; // 年齢
-
-                        // ツール費用の計算
-                        $toolcost = 0;
-                        foreach($_POST["tool-cost"] as $key => $value){
-                            $tools = Tools::where('name', '=', $value)->get();
-                            foreach($tools as $tool){
-                                $price = $tool->price;
-                                $toolcost += $price;
-                            }
-                        }
-
-                        // 社会保険料の計算の準備
-                        $base = $monthly+$traffic; // 基準となる金額の算出
-                        $insurances = Insurance::select('salary')->get(); // 標準報酬のカラムのみ取得
-
-                        // 月給と標準報酬を比較して最適な等級(=ID)を見つけ出す
-                        $id = 0;
-                        $next = 0;
-                        foreach($insurances as $salary){
-                            $next = $salary->salary;
-                            if($base < $next){
-                                break;
-                            }
-                            $id++;
-                        }
-
-                        // 見つけ出したIDで今度はその行のみ取得
-                        $insurances2 = Insurance::find($id);
-
-                        // 社会保険料と雇用保険料の計算
-                        if($age >= 40){
-                            $health = $insurances2->health_care;
-                        } else {
-                            $health = $insurances2->health;
-                        }
-                        $fraction = $health - (int)$health;
-                        if($fraction > 0){
-                            $health += 1;
-                        }
-                        $health = (int)$health;
-
-                        $welfare = $insurances2->welfare;
-                        $fraction = $welfare - (int)$welfare;
-                        if($fraction > 0){
-                            $welfare += 1;
-                        }
-                        $welfare = (int)$welfare;
-
-                        $employment = $monthly*0.0095;
-                        $fraction = $employment - (int)$employment;
-                        if($fraction >= 0.5){
-                            $employment += 1;
-                        }
-                        $employment = (int)$employment;
-
-                        // 初期費用とランニングコストを計算
-                        $result = $base + $health + $welfare + $employment + $toolcost;
-                        $first = $result + $_POST["equipment-cost"];
-                    }
-                    //パートタイムの時の処理
-                    else{
-                        // dd($_POST);
-
-                        // 入力されたデータの取得
+                    } else {
+                        // パートタイム
                         $monthly = $_POST["hourly-wage"] *$_POST["hours-per-day"]* $_POST["days-per-week"]*4; // 月給(1ヶ月4週間とする))
                         $traffic = $_POST["transport-cost"]* $_POST["days-per-week"]*4; // 1ヶ月の交通費
                         $age = $_POST["age"]; // 年齢
-
-                        // ツール費用の計算
-                        $toolcost = 0;
-                        foreach($_POST["tool-cost"] as $key => $value){
-                            $tools = Tools::where('name', '=', $value)->get();
-                            foreach($tools as $tool){
-                                $price = $tool->price;
-                                $toolcost += $price;
-                            }
-                        }
-
-                        // 社会保険料の計算の準備
-                        $base = $monthly+$traffic; // 基準となる金額の算出
-                        $insurances = Insurance::select('salary')->get(); // 標準報酬のカラムのみ取得
-
-                        // 月給と標準報酬を比較して最適な等級(=ID)を見つけ出す
-                        $id = 0;
-                        $next = 0;
-                        foreach($insurances as $salary){
-                            $next = $salary->salary;
-                            if($base < $next){
-                                break;
-                            }
-                            $id++;
-                        }
-
-                        // 見つけ出したIDで今度はその行のみ取得
-                        $insurances2 = Insurance::find($id);
-
-                        // 社会保険料と雇用保険料の計算
-                        if($age >= 40){
-                            $health = $insurances2->health_care;
-                        } else {
-                            $health = $insurances2->health;
-                        }
-                        $fraction = $health - (int)$health;
-                        if($fraction > 0){
-                            $health += 1;
-                        }
-                        $health = (int)$health;
-
-                        $welfare = $insurances2->welfare;
-                        $fraction = $welfare - (int)$welfare;
-                        if($fraction > 0){
-                            $welfare += 1;
-                        }
-                        $welfare = (int)$welfare;
-
-                        $employment = $monthly*0.0095;
-                        $fraction = $employment - (int)$employment;
-                        if($fraction >= 0.5){
-                            $employment += 1;
-                        }
-                        $employment = (int)$employment;
-
-                        // 初期費用とランニングコストを計算
-                        $result = $base + $health + $welfare + $employment + $toolcost;
-                        $first = $result + $_POST["equipment-cost"];
                     }
+
+                    // ツール費用の計算
+                    $toolcost = 0;
+                    foreach($_POST["tool-cost"] as $key => $value){
+                        $tools = Tools::where('name', '=', $value)->get();
+                        foreach($tools as $tool){
+                            $price = $tool->price;
+                            $toolcost += $price;
+                        }
+                    }
+
+                    // 社会保険料の計算の準備
+                    $base = $monthly+$traffic; // 基準となる金額の算出
+                    $insurances = Insurance::select('salary')->get(); // 標準報酬のカラムのみ取得
+
+                    // 月給と標準報酬を比較して最適な等級(=ID)を見つけ出す
+                    $id = 0;
+                    $next = 0;
+                    foreach($insurances as $salary){
+                        $next = $salary->salary;
+                        if($base < $next){
+                            break;
+                        }
+                        $id++;
+                    }
+
+                    // 見つけ出したIDで今度はその行のみ取得
+                    $insurances2 = Insurance::find($id);
+
+                    // 社会保険料と雇用保険料の計算
+                    if($age >= 40){
+                        $health = $insurances2->health_care;
+                    } else {
+                        $health = $insurances2->health;
+                    }
+                    $fraction = $health - (int)$health;
+                    $health2 = $health;
+                    if($fraction > 0){
+                        $health += 1;
+                    }
+                    $health = (int)$health;
+                    $health2 = (int)$health2;
+
+                    $welfare = $insurances2->welfare;
+                    $welfare2 = $welfare;
+                    $fraction = $welfare - (int)$welfare;
+                    if($fraction > 0){
+                        $welfare += 1;
+                    }
+                    $welfare = (int)$welfare;
+                    $welfare2 = (int)$welfare2;
+
+                    $employment = $monthly*0.0095;
+                    $fraction = $employment - (int)$employment;
+                    if($fraction >= 0.5){
+                        $employment += 1;
+                    }
+                    $employment = (int)$employment;
+
+                    // 初期費用とランニングコストを計算
+                    $result = $base + $health + $welfare + $employment + $toolcost;
+                    $first = $result + $_POST["equipment-cost"];
+
+                    // 手取りの計算
+                    $employment2 = $monthly*0.006;
+                    $fraction = $employment2 - (int)$employment2;
+                    if($fraction > 0.5){
+                        $employment2 += 1;
+                    }
+                    $employment2 = (int)$employment2;
+
+                    $social = $health2 + $welfare2 + $employment2; // 控除額の計算
+                    $income = $monthly - $social; // 月給から控除額を排除
+
                     $employmentType = $_POST["employment-type"]; // 変数名を修正
                 ?>
-
 
                 <!-- 出力 -->
                 <p>【かかる費用】</p>
@@ -161,6 +112,7 @@ use App\Models\Insurance;
 
                 <p>【詳細】</p>
                 <p>月給：<?= ($monthly/10000) ?>万円</p>
+                <p>(手取り：<?= $income ?>円)※税金は現状未反映</p>
                 <p>月毎の交通費：<?= $traffic ?>円</p>
                 <br>
 
@@ -188,9 +140,6 @@ use App\Models\Insurance;
                 <div class="mt-6">
                     <h3 class="text-lg font-semibold text-gray-800 dark:text-gray-200">雇用形態: {{ $employmentType === 'fulltime' ? 'フルタイム' : 'パートタイム' }}</h3>
                 </div>
-                
-                <p>手取り</p>
-
             </div>
         </div>
     </div>
